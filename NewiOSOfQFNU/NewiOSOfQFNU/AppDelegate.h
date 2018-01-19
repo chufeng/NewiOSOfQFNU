1d15221c62bb757b7922f0248facfac1e2395fb0
yida <492818574@qq.com>
2018-01-19 15:14:46 +0800

    begin

---
+++ NewiOSOfQFNU/NewiOSOfQFNU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewiOSOfQFNU
+//
+//  Created by lyngame on 2018/1/18.
+//  Copyright © 2018年 yida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewiOSOfQFNU/NewiOSOfQFNU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewiOSOfQFNU
+//
+//  Created by lyngame on 2018/1/18.
+//  Copyright © 2018年 yida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
